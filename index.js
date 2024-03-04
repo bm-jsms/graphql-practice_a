399db31a1d93cdf64a8ce0d8f113ca57409fb40d
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-04 17:28:12 -0500

    add: data

---
+++ index.js
@@ -0,0 +1,23 @@
+const persons = [
+	{
+		name: 'John',
+		phone: '123-456-7890',
+		street: '1234 Main St',
+		city: 'San Francisco',
+		id: 1,
+	},
+	{
+		name: 'Jane',
+		phone: '123-456-7890',
+		street: '1234 Main St',
+		city: 'San Francisco',
+		id: 2,
+	},
+	{
+		name: 'Doe',
+		phone: '123-456-7890',
+		street: '1234 Main St',
+		city: 'San Francisco',
+		id: 3,
+	},
+];
